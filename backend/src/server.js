662fa2295d4fdb596259b53be87bdcbc4332951c
naykakashima <porgimat@gmail.com>
2025-10-02 19:54:03 +0100

    establish db connection

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,10 +1,19 @@
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
+import { connectDB } from './config/db.js';
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+
+console.log(process.env.PORT);
+
+console.log(process.env.MONGO_URI);
+
+connectDB();
 
 app.use("/api/notes", notesRoutes)
 
-app.listen(5001, () => {
+app.listen(PORT, () => {
     console.log('Server is running on port 5001');
 });
+
